f5c747e4db62c99bb47379fb767e62122cf1c9e7
sanleriver-IAS <sanleriver@gmail.com>
2022-03-08 14:47:23 -0500

    metodos http del controlador

---
+++ src/main/java/com/example/semillerodia1/Semillerodia1Application.java
@@ -0,0 +1,13 @@
+package com.example.semillerodia1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semillerodia1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semillerodia1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/semillerodia1/controller/Controller.java
@@ -0,0 +1,30 @@
+package com.example.semillerodia1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping(path = "/products", method = RequestMethod.GET)
+    public String getProducts(){
+        return "Este es el método para consultar todos los productos";
+    }
+
+    @RequestMapping(path = "/products", method = RequestMethod.POST)
+    public String postProducts(){
+        return "Este es el método para crear un producto";
+    }
+
+    @RequestMapping(path = "/products", method = RequestMethod.PUT)
+    public String putProducts(){
+        return "Este es el método para actualizar un producto";
+    }
+
+    @RequestMapping(path = "/products", method = RequestMethod.DELETE)
+    public String deleteProducts(){
+        return "Este es el método para eliminar un producto";
+    }
+
+}
